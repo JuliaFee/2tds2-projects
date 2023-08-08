7d450f7023c7dc0a8a92ced205deb3e18e29c350
JuliaFee <julia.silva133@aluno.senai.br>
2023-08-08 16:23:53 -0300

    feat: create edit post function

--- assets/js/scripts.posts.js
+++ assets/js/scripts.posts.js
@@ -1,5 +1,5 @@
 const posts = [];
-
+let postIndex = -1;
 
 function savePost(){
     const title = document.getElementById("title").value;
@@ -8,16 +8,31 @@ function savePost(){
     const author = document.getElementById("author").value;
     const date = document.getElementById("date").value;
 
+    
+
     if (title && category && resume && author && date ){
+        if (postIndex == -1){
     storePost(title, category, resume, author, date);
     cleanFields();
     showPosts();
     } else {
-        alert("Preencha todos os campos!");
+        post[postIndex] = {
+            title,
+            category, 
+            resume,
+            author,
+            date,
+        };
     }
     
-   
+    cleanFields();
+    showPosts();
+    postIndex = -1
+    } else {
+        alert("Preencha todos os campos!");
+    }
 }
+    
 function cleanFields(){
     const title = document.getElementById("title").value = "";
     const category = document.getElementById("category").value = "";
@@ -45,18 +60,30 @@ function showPosts(){
     posts.forEach((post, index) =>{
     showContent += `
     <div class="itemPost">
-    <h2>${post.title}</h2>
-    <p><strong>Categoria: </strong>${post.category}</p>
-    <p><strong>Resumo: </strong>${post.resume}</p>
-    <p><strong>Autor: </strong>${post.author}</p>
-    <p><strong>Data de publicação: </strong>${post.date}</p>
+        <h2>${post.title}</h2>
+        <p><strong>Categoria:</strong>${post.category}</p>
+        <p><strong>Resumo:</strong>${post.resume}</p>
+        <p><strong>Autor:</strong>${post.author}</p>
+        <p><strong>Data de publicação:</strong>${post.date}</p>
 
-    <button onclick="editPost(${index})>Editar</button>
-    <button onclick="deletePost(${index})>Excluir</button>
-       
-    </div>`;
+        <button onclick="editPost(${index})">Editar</button>
+        <button onclick="deletPost(${index})">Excluir</button>
+
+    </div>
+    `;
     })
 
     document.getElementById("list").innerHTML = showContent;
 
+}
+function editPost(index){
+    const post = posts[index];
+
+    document.getElementById("title").value = post.title;
+    document.getElementById("category").value = post.cateory;
+    document.getElementById("resume").value = post.resume;
+    document.getElementById("author").value = post.author;
+    document.getElementById("date").value = post.date;
+
+    postIndex = index;
 }
