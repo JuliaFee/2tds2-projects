0a06682765881c45686d8ebbc26ab5bc80b4d9d7
JuliaFee <julia.silva133@aluno.senai.br>
2023-08-17 10:31:36 -0300

    feat: create category service

--- assets/js/category-product.js
+++ assets/js/category-product.js
@@ -7,5 +7,24 @@ class Category {
     }
 }
 
+class Product{
+    constructor(id, name, price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
 
+class CategoryService{
+    constructor(){
+        this.categories = [];
+        this.nextCategoryId = 0;
+    }
 
+    AddCategory(name){
+        const id = this.nextCategoryId++;
+        const category = new Category(id, name);
+        this.categories.push(category);
+    }
+}
