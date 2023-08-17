fde682c65e7f87dac3ad3421989588693f52c893
JuliaFee <julia.silva133@aluno.senai.br>
2023-08-17 10:17:59 -0300

    feat: add class product

--- assets/js/category-product.js
+++ assets/js/category-product.js
@@ -1,8 +1,11 @@
+// categoria possui varios produtos
 class Category {
     constructor (id, name){
         this.name = name;
         this.id = id;
+        this.products = [];
     }
 }
 
-// categoria possui varios produtos
+
+
